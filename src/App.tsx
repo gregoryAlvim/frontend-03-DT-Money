677d2f5af0bd9c2b387e2078f746b0dfc6f2b0b6
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-04 10:03:15 -0300

    Feat: created App and added ThemeProvider, GlobalStyle and Transactions component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Transactions />
+    </ThemeProvider>
+  )
+}
