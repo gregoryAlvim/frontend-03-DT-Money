a48a239ceeb47e4345c43cdedbe870d1dbb4956f
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-05 12:41:31 -0300

    Feat: added use context selector

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,16 @@
-import * as S from "./styles";
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { SearchForm } from "./components/SearchForm/Index";
+import * as S from './styles'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { useContextSelector } from 'use-context-selector'
+import { SearchForm } from './components/SearchForm/Index'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 export function Transactions() {
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
+
   return (
     <div>
       <Header />
@@ -11,21 +18,22 @@ export function Transactions() {
 
       <S.TransactionsContainer>
         <SearchForm />
-        
+
         <S.TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td><S.PriceHighLight variant="income"> R$ 12.000,00 </S.PriceHighLight></td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td><S.PriceHighLight variant="expense"> - R$ 59,90 </S.PriceHighLight></td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <S.PriceHighLight variant={transaction.type}>
+                    {transaction.type === 'expense' && '- '}
+                    {priceFormatter.format(transaction.price)}
+                  </S.PriceHighLight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+              </tr>
+            ))}
           </tbody>
         </S.TransactionsTable>
       </S.TransactionsContainer>
