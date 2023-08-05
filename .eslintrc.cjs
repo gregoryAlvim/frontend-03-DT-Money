e1840593eb89054baf1360514705b027c7659f04
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-05 12:34:38 -0300

    Build: added new dependencies and configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +12,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
   },
 }
