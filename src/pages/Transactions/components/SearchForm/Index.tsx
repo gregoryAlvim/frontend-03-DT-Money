24bc2fd425d9d1e6a5cd5f6ec69bd083a71b77a5
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-05 12:42:05 -0300

    Feat: added react hook form on form and added use context selector to context on fetch trnansactions

--- src/pages/Transactions/components/SearchForm/Index.tsx
+++ src/pages/Transactions/components/SearchForm/Index.tsx
@@ -1,12 +1,46 @@
-import * as S from "./styles";
-import { MagnifyingGlass } from "phosphor-react";
+import * as z from 'zod'
+import * as S from './styles'
+import { useForm } from 'react-hook-form'
+import { MagnifyingGlass } from 'phosphor-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContextSelector } from 'use-context-selector'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  return (
-    <S.SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
-      <button type="submit">
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <S.SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass />
         Buscar
       </button>
