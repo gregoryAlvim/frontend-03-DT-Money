08024ed187a9e08225392f68d7715f9233d494c3
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-05 12:35:03 -0300

    Feat: added TRansactionsProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { GlobalStyle } from "./styles/global";
-import { ThemeProvider } from "styled-components";
-import { Transactions } from "./pages/Transactions";
-import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
+import { defaultTheme } from './styles/themes/default'
+import { TransactionsProvider } from './contexts/TransactionsContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transactions />
+
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
